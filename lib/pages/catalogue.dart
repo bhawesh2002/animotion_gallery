122e3a7a0ad46f6bc4ff04aea83d73050e6c89c5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-26 19:06:39 +0530

    Add Catalogue page

---
+++ lib/pages/catalogue.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Catalogue extends StatelessWidget {
+  const Catalogue({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold());
+  }
+}
