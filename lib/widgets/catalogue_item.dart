ad84b5915952a1829d5c1c9677a8558a5e6840fa
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-27 10:54:30 +0530

    Add CatalogueItem widget

---
+++ lib/widgets/catalogue_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CatalogueItem extends StatelessWidget {
+  final String itemLabel;
+  final Widget widget;
+  const CatalogueItem(
+      {super.key, required this.itemLabel, required this.widget});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(10),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        onTap: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => widget));
+        },
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: MediaQuery.of(context).size.height * 0.02,
+                horizontal: MediaQuery.of(context).size.width * 0.05),
+            child: Text(
+              itemLabel,
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.040,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
