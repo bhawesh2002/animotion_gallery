2eab3515c5821d8e94de22e0016ef27a19005cb4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-29 09:40:39 +0530

    Add AniPaddingPage and AniPadding classes for animated padding

---
+++ lib/implicit animations/ani_padding.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class AniPaddingPage extends StatelessWidget {
+  const AniPaddingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              border: Border.all(width: 2, color: Colors.black),
+            ),
+            child: const AniPadding(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AniPadding extends StatefulWidget {
+  const AniPadding({super.key});
+
+  @override
+  State<AniPadding> createState() => _AniPaddingState();
+}
+
+class _AniPaddingState extends State<AniPadding> {
+  EdgeInsetsGeometry padding = const EdgeInsets.all(10);
+
+  void animatePadding() {
+    double left = Random().nextDouble() * 150;
+    double right = Random().nextDouble() * 150;
+    double top = Random().nextDouble() * 150;
+    double bottom = Random().nextDouble() * 150;
+    setState(() {
+      padding =
+          EdgeInsets.only(left: left, right: right, top: top, bottom: bottom);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPadding(
+      padding: padding,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.decelerate,
+      child: GestureDetector(
+        onTap: animatePadding,
+        child: const FlutterLogo(),
+      ),
+    );
+  }
+}
