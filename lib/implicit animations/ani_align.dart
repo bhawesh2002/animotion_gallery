cfe2377f123e3bdd80c52356e6adc112b9061319
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-30 17:14:19 +0530

    Add AniAlignPage and AniAlign widget

---
+++ lib/implicit animations/ani_align.dart
@@ -0,0 +1,105 @@
+import 'dart:math';
+
+import 'package:animotion_gallery/widgets/back_arrow.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AniAlignPage extends StatelessWidget {
+  const AniAlignPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            top: 10,
+            left: 10,
+            child:
+                Align(alignment: Alignment.topLeft, child: backArrow(context)),
+          ),
+          const Positioned.fill(
+              top: 100,
+              right: 25,
+              left: 25,
+              bottom: 200,
+              child: Align(alignment: Alignment.center, child: AniAlign())),
+          const Positioned.fill(
+            bottom: 100,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: 300,
+                child: Text(
+                  "Tap on Container change its alignment.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class AniAlign extends StatefulWidget {
+  const AniAlign({super.key});
+
+  @override
+  State<AniAlign> createState() => _AniAlignState();
+}
+
+class _AniAlignState extends State<AniAlign> {
+  Alignment alignment = Alignment.center;
+
+  void animateAlignment() {
+    setState(() {
+      alignment = Alignment((Random().nextInt(3) - 1).toDouble(),
+          (Random().nextInt(3) - 1).toDouble());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 2),
+            color: Colors.grey.shade200,
+          ),
+        ),
+        Positioned.fill(
+          child: AnimatedAlign(
+            duration: const Duration(milliseconds: 350),
+            alignment: alignment,
+            curve: Curves.easeInOutCubic,
+            child: GestureDetector(
+              onTap: animateAlignment,
+              child: SizedBox(
+                width: 90,
+                height: 90,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const FlutterLogo(
+                      size: 60,
+                    ),
+                    const SizedBox.square(
+                      dimension: 10,
+                    ),
+                    Text("(${alignment.x}, ${alignment.y})")
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
