e963229a05d1b903aed73cc959a1b89d5b19cf24
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-02 19:24:06 +0530

    Add graph visibility toggle and expand/collapse functionality

--- lib/widgets/palyground.dart
+++ lib/widgets/palyground.dart
@@ -9,9 +9,31 @@ class Playground extends StatefulWidget {
 
 class _PlaygroundState extends State<Playground> {
   Offset _offset = Offset.zero;
+  bool graphVisible = true;
+  bool moreExpanded = false;
+  double resetBottomPos = 10;
+  double visibleBottomPos = 10;
+  double scale = 1;
+  double rotation = 0;
   void _resetOffset() {
     setState(() {
       _offset = Offset.zero;
+    });
+  }
+
+  void toggleGraphVisibility() {
+    setState(() {
+      graphVisible = graphVisible ? false : true;
+    });
+  }
+
+  void toggleExpanded() {
+    setState(() {
+      moreExpanded = moreExpanded ? false : true;
+      resetBottomPos = moreExpanded ? 60 : 10;
+      visibleBottomPos = moreExpanded ? 110 : 10;
+      scale = moreExpanded ? 1 : 0;
+      rotation = moreExpanded ? 0.5 : 0;
     });
   }
 
@@ -33,26 +55,78 @@ class _PlaygroundState extends State<Playground> {
                 decoration: BoxDecoration(
                   border: Border.all(width: 2, color: Colors.grey),
                 ),
-                child: CustomPaint(
-                  painter: GraphPainter(offset: _offset),
-                  size: Size.infinite,
-                ),
+                child: graphVisible
+                    ? CustomPaint(
+                        painter: GraphPainter(offset: _offset),
+                        size: Size.infinite,
+                      )
+                    : null,
               );
             },
           ),
         ),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            margin: const EdgeInsets.all(14.0),
-            child: IconButton(
-              style: IconButton.styleFrom(
-                backgroundColor: Colors.white,
-                elevation: 2,
-                shadowColor: Colors.black,
+        AnimatedPositioned(
+          right: 10,
+          bottom: visibleBottomPos,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          child: AnimatedScale(
+            scale: scale,
+            duration: const Duration(milliseconds: 300),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: IconButton(
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  elevation: 2,
+                  shadowColor: Colors.black,
+                ),
+                onPressed: toggleGraphVisibility,
+                icon: const Icon(Icons.visibility_outlined),
               ),
-              onPressed: _resetOffset,
-              icon: const Icon(Icons.refresh),
+            ),
+          ),
+        ),
+        AnimatedPositioned(
+          right: 10,
+          bottom: resetBottomPos,
+          curve: Curves.easeInOut,
+          duration: const Duration(milliseconds: 200),
+          child: AnimatedScale(
+            scale: scale,
+            duration: const Duration(milliseconds: 300),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: IconButton(
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  elevation: 2,
+                  shadowColor: Colors.black,
+                ),
+                onPressed: _resetOffset,
+                icon: const Icon(Icons.refresh_rounded),
+              ),
+            ),
+          ),
+        ),
+        Positioned.fill(
+          right: 10,
+          bottom: 10,
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: AnimatedRotation(
+              turns: rotation,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              child: IconButton(
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  elevation: 2,
+                  shadowColor: Colors.black,
+                ),
+                onPressed: toggleExpanded,
+                icon: const Icon(Icons.keyboard_arrow_up_rounded),
+              ),
             ),
           ),
         ),
