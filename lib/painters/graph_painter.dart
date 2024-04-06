ef2977dfe178151963823a419a0313047d82d815
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-06 16:20:34 +0530

    Add GraphPainter class for grid painting functionality

---
+++ lib/painters/graph_painter.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class GraphPainter extends CustomPainter {
+  final Offset offset;
+
+  GraphPainter({super.repaint, required this.offset});
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double width = size.width;
+    final double height = size.height;
+    final double gridSpacing =
+        width / 10; // You can adjust this spacing as needed
+
+    final Paint paint = Paint()
+      ..color = Colors.grey[400]!
+      ..strokeWidth = 1;
+
+    // Adjust the starting point of the grid based on offset
+    final double startX = offset.dx % gridSpacing;
+    final double startY = offset.dy % gridSpacing;
+
+    // Draw horizontal lines
+    for (double y = startY; y <= height; y += gridSpacing) {
+      canvas.drawLine(Offset(0, y), Offset(width, y), paint);
+    }
+
+    // Draw vertical lines
+    for (double x = startX; x <= width; x += gridSpacing) {
+      canvas.drawLine(Offset(x, 0), Offset(x, height), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(GraphPainter oldDelegate) {
+    return offset != oldDelegate.offset;
+  }
+}
--- lib/widgets/playground/palyground.dart
+++ lib/widgets/playground/palyground.dart
@@ -1,3 +1,4 @@
+import 'package:animotion_gallery/painters/graph_painter.dart';
 import 'package:flutter/material.dart';
 
 class Playground extends StatefulWidget {
@@ -132,41 +133,5 @@ class _PlaygroundState extends State<Playground> {
         ),
       ],
     );
-  }
-}
-
-class GraphPainter extends CustomPainter {
-  final Offset offset;
-
-  GraphPainter({super.repaint, required this.offset});
-  @override
-  void paint(Canvas canvas, Size size) {
-    final double width = size.width;
-    final double height = size.height;
-    final double gridSpacing =
-        width / 10; // You can adjust this spacing as needed
-
-    final Paint paint = Paint()
-      ..color = Colors.grey[400]!
-      ..strokeWidth = 1;
-
-    // Adjust the starting point of the grid based on offset
-    final double startX = offset.dx % gridSpacing;
-    final double startY = offset.dy % gridSpacing;
-
-    // Draw horizontal lines
-    for (double y = startY; y <= height; y += gridSpacing) {
-      canvas.drawLine(Offset(0, y), Offset(width, y), paint);
-    }
-
-    // Draw vertical lines
-    for (double x = startX; x <= width; x += gridSpacing) {
-      canvas.drawLine(Offset(x, 0), Offset(x, height), paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(GraphPainter oldDelegate) {
-    return offset != oldDelegate.offset;
   }
 }
