640f6dd6b3da09c6fa09de5876e2ac88cfe361ff
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-07 21:39:27 +0530

    Add expand/collapse functionality to MoreOptions widget

--- lib/widgets/playground/more_options.dart
+++ lib/widgets/playground/more_options.dart
@@ -1,15 +1,108 @@
 import 'package:flutter/material.dart';
 
 class MoreOptions extends StatefulWidget {
-  const MoreOptions({super.key});
+  final double? left;
+  final double? right;
+  final double? top;
+  final double? bottom;
+  final Alignment? alignment;
+  const MoreOptions(
+      {super.key,
+      this.alignment,
+      this.left,
+      this.right,
+      this.top,
+      this.bottom});
 
   @override
   State<MoreOptions> createState() => _MoreOptionsState();
 }
 
 class _MoreOptionsState extends State<MoreOptions> {
+  bool moreExpanded = false;
+  double rotation = 0;
+  double scale = 1;
+  double resetBottomPos = 10;
+  double visibleBottomPos = 10;
+  void toggleExpanded() {
+    setState(() {
+      moreExpanded = moreExpanded ? false : true;
+      resetBottomPos = moreExpanded ? 60 : 10;
+      visibleBottomPos = moreExpanded ? 110 : 10;
+      rotation = moreExpanded ? 0.5 : 0;
+      scale = moreExpanded ? 1 : 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Stack();
+    return Stack(
+      children: [
+        AnimatedPositioned(
+          right: 10,
+          bottom: visibleBottomPos,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          child: AnimatedScale(
+            scale: scale,
+            duration: const Duration(milliseconds: 300),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: IconButton(
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  elevation: 2,
+                  shadowColor: Colors.black,
+                ),
+                onPressed: () {},
+                icon: const Icon(Icons.visibility_outlined),
+              ),
+            ),
+          ),
+        ),
+        AnimatedPositioned(
+          right: 10,
+          bottom: resetBottomPos,
+          curve: Curves.easeInOut,
+          duration: const Duration(milliseconds: 200),
+          child: AnimatedScale(
+            scale: scale,
+            duration: const Duration(milliseconds: 300),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: IconButton(
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  elevation: 2,
+                  shadowColor: Colors.black,
+                ),
+                onPressed: () {},
+                icon: const Icon(Icons.refresh_rounded),
+              ),
+            ),
+          ),
+        ),
+        Positioned.fill(
+          right: widget.right,
+          bottom: widget.bottom,
+          left: widget.left,
+          top: widget.top,
+          child: AnimatedRotation(
+            turns: rotation,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            child: IconButton(
+              style: IconButton.styleFrom(
+                backgroundColor: Colors.white,
+                elevation: 2,
+                shadowColor: Colors.black,
+              ),
+              onPressed: toggleExpanded,
+              icon: const Icon(Icons.keyboard_arrow_up_rounded),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
