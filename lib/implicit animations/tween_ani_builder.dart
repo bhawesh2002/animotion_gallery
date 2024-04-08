abbfb94d859bc60040ac9550549c73447ef71bfb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-08 19:04:09 +0530

    Update TweenAniBuilderPage widget to include Playground widget with TweenAniBuilder child and update UI layout and import statements

--- lib/implicit animations/tween_ani_builder.dart
+++ lib/implicit animations/tween_ani_builder.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:animotion_gallery/widgets/back_arrow.dart';
+import 'package:animotion_gallery/widgets/playground/palyground.dart';
 import 'package:flutter/material.dart';
 
 class TweenAniBuilderPage extends StatelessWidget {
@@ -10,28 +10,35 @@ class TweenAniBuilderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Tween Animation Builder"),
+        ),
         body: Stack(
           children: [
             Positioned.fill(
-              top: 10,
-              left: 10,
+              top: 20,
               child: Align(
-                  alignment: Alignment.topLeft, child: backArrow(context)),
-            ),
-            const Center(
-              child: TweenAniBuilder(),
-            ),
-            const Positioned.fill(
-              bottom: 100,
-              child: Align(
-                alignment: Alignment.bottomCenter,
+                alignment: Alignment.topCenter,
                 child: SizedBox(
-                  width: 350,
-                  child: Text(
-                    "Tap on the container to see it grow and shrink",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18),
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  child: Playground(
+                    child: const TweenAniBuilder(),
                   ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.05,
+                  vertical: MediaQuery.of(context).size.height * 0.03,
+                ),
+                child: const Text(
+                  "Tap on the Container to see it Grow and Shrink.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18),
                 ),
               ),
             ),
