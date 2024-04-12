7f7c41f1c62576b11935fd66a06bc00fe2e250da
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-12 22:00:32 +0530

    Create PlayButton

---
+++ lib/widgets/play_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class PlayButton extends StatefulWidget {
+  final VoidCallback action;
+  const PlayButton({super.key, required this.action});
+
+  @override
+  State<PlayButton> createState() => _PlayButtonState();
+}
+
+class _PlayButtonState extends State<PlayButton> {
+  bool played = false;
+  void playPressed() {
+    setState(() {
+      played = !played;
+      debugPrint("playing : $played");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      transitionBuilder: (child, animation) {
+        return ScaleTransition(
+          scale: animation,
+          child: child,
+        );
+      },
+      child: IconButton(
+        key: ValueKey<bool>(played),
+        style: IconButton.styleFrom(
+          backgroundColor: Colors.white,
+          shadowColor: Colors.black,
+          elevation: 2,
+          iconSize: 40,
+        ),
+        onPressed: () {
+          playPressed();
+        },
+        icon: Icon(played ? Icons.play_arrow : Icons.pause),
+      ),
+    );
+  }
+}
