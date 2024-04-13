f7508d956822d2d70f90dee70ec00b587185a0bf
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-13 21:39:30 +0530

    Add EditBottomSheet widget

---
+++ lib/widgets/edit_bottom_sheet.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class EditBottomSheet extends StatefulWidget {
+  const EditBottomSheet({super.key});
+
+  @override
+  State<EditBottomSheet> createState() => _EditBottomSheetState();
+}
+
+class _EditBottomSheetState extends State<EditBottomSheet>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomSheet(
+      animationController: _animationController,
+      onClosing: () {},
+      builder: (context) {
+        return const Center(
+          child: Text("Edit Botton Sheet"),
+        );
+      },
+    );
+  }
+}
